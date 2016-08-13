b9c4bf4c23ea9af383d3b07b282814674eb6cbd8
Lee Byron <lee@leebyron.com>
2016-08-13 13:38:35 -0700

    update to latest v of async-to-gen

--- test/expected.js
+++ test/expected.js
@@ -1,14 +1,14 @@
 'use strict';
 
-function __async(f){var g=f();return new Promise(function(s,j){function c(a,x){try{var r=g[x?"throw":"next"](a)}catch(e){return j(e)}return r.done?s(r.value):Promise.resolve(r.value).then(c,d)}function d(e){return c(e,1)}c()})}
+function __async(g){return new Promise(function(s,j){function c(a,x){try{var r=g[x?"throw":"next"](a)}catch(e){return j(e)}return r.done?s(r.value):Promise.resolve(r.value).then(c,d)}function d(e){return c(e,1)}c()})}
 
 function mystery() {return __async(function*(){
   return yield 'oOOoooOOOooo'
-})}
+}())}
 
 // async function statement
 function foo() {return __async(function*(){
   return yield mystery()
-})}
+}())}
 
 foo().then(console.log)
